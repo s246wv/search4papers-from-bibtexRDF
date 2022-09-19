5f3674128f8803e3ee8700d68d3df63f74e5c013
s246wv <satoshi.nishimura.3194@gmail.com>
2022-09-19 15:35:50 +0900

    m

---
+++ backend/utils/getKeywords.py
@@ -0,0 +1,25 @@
+from rdflib import Graph
+from string import Template
+import json
+
+def getKeywords(value, url):
+    graph = Graph()
+    # The 'publicID' is unofficial base URI.
+    graph.parse(url)
+    # Please modify this SPARQL query if you want to change the resource
+    t = Template("""
+        prefix skos: <http://www.w3.org/2004/02/skos/core#>
+        select ?child ?childLabel where {
+        ?parent skos:narrower ?child.
+        ?child skos:prefLabel ?childLabel.
+        filter (?parent = <${first}> )
+        }
+    """)
+    query1 = t.substitute(first=parent)
+    qres1 = graph.query(query1)
+    ret = []
+    for row in qres1:
+        dict = {row[0].toPython(): row[1].toPython()}
+        ret.append(dict)
+
+    return ret
--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,18 +1,43 @@
 /* eslint no-unused-expressions: "off" */
 import React from 'react';
-import { Input, Space, TreeSelect, TreeSelectProps } from 'antd';
+import { Input, TreeSelect, Table } from 'antd';
 import 'antd/dist/antd.css';
 import Axios from 'axios';
+import type { ColumnsType } from 'antd/lib/table';
 
 //import './App.css';
 
+interface TableDataType {
+  key: React.Key;
+  title: string;
+  keyword: number;
+  ACM_keyword: string;
+  doi: string;
+}
 
+const columns: ColumnsType<TableDataType> = [
+  {
+    title: "Title",
+    dataIndex: "title",
+    filtered: true,
+  },
+  {
+    title: "Keywords",
+    dataIndex: "keyword",
+    filtered: true,
+  },
+  {
+    title: "ACM Keyword",
+    dataIndex: "ACM_keyword",
+  }
+]
 
 function App() {
   const { Search } = Input
   const [value, setValue] = React.useState();
   const [treeData, setTreeData] = React.useState<Object[]>([]);
-  const [bibtexData, setBibtexData] = React.useState<Object[]>([]);
+  const [bibtexUrl, setBibtexUrl] = React.useState<string>();
+  const [tableData, setTableData] = React.useState<TableDataType[]>([]);
 
   React.useEffect(() => {
     Axios.get("http://localhost:5000/getRoot").then((res) => {
@@ -36,12 +61,14 @@ function App() {
   }, []);
 
   const onLoad = (value: string) => {
+    // こっちをURL保持にとどめるか．先に処理出来るものは裏で処理したいのだけれど．．
     console.log(value);
-    Axios.post("http://localhost:5000/onLoad", {
-      url: value
-    }).then((res: any) => {
-      //alert(res)
-    })
+    setBibtexUrl(value);
+    // Axios.post("http://localhost:5000/onLoad", {
+    //   url: value
+    // }).then((res: any) => {
+    //   //alert(res)
+    // })
   };
 
   const getChildrenNodes = async (parent: string) => {
@@ -81,10 +108,10 @@ function App() {
           const changingElement = treeData.filter((element: any) => {
             return element.id === id;
           })[0];
-          const changedElement = {...changingElement, isLeaf: true};
+          const changedElement = { ...changingElement, isLeaf: true };
           // 並び順が変わってしまう．．
           setTreeData([...remainedList, changedElement]);
-        // 展開して子があったときの処理．単にtreeDataに足す．
+          // 展開して子があったときの処理．単にtreeDataに足す．
         } else {
           setTreeData(
             treeData.concat(children),
@@ -95,15 +122,25 @@ function App() {
       }, 300);
     });
 
+  const getKeywords = async (value: string) => {
+    await Axios.post("http://localhost:5000/getKeywords", { value: value }).then((res) => {
+      const response = res.data;
+      console.log(response);
+
+    }).catch((err) => {
+      alert(err);
+    });
+  }
 
   const onChange = (newValue: any) => {
     // embeddingをもらって比較するのか？
-    // いったんSPARQLだけにしましょう．
+    // いったんSPARQLだけにしましょう．->bibtexDataにもらっているのか．
     // getKeywordsして，setする．setされてたらTreeSelectの下にTableを出して，getKeywordsの結果を表示する．
     console.log(newValue);
     console.log(treeData);
     setValue(newValue);
   };
+
   return (
     <div className="App" style={{ margin: 100, width: 500 }}>
       <header className="App-header">
@@ -125,6 +162,12 @@ function App() {
           loadData={onLoadData}
           treeData={treeData}
         />
+        {tableData.length > 0 &&
+          <Table
+            columns={columns}
+            dataSource={tableData}
+          />
+        }
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
